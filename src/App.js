14d0c16c513491aadd92b91a3716d5e12c8d4333
Nikita <69010558+Kapibarra@users.noreply.github.com>
2020-12-03 01:06:20 +0300

    123

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import { Fragment } from "react";
 import "./App.css";
-import Header from "./components/Header/Header";
-import Main from "./components/Main/Main";
 import Layout from "./Hoc/Layout/Layout";
 
 function App() {
