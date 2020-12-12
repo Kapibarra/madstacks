7c4153f8f28c78591026d50187a3e013449be883
Nikita <69010558+Kapibarra@users.noreply.github.com>
2020-12-12 22:17:54 +0300

    fixed portfolio component

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -3,9 +3,11 @@ import classes from "./Portfolio.module.css"
 import AwesomeSlider from 'react-awesome-slider';
 import 'react-awesome-slider/dist/styles.css';
 
+
+
 const Slider = () => {
     return (
-        <AwesomeSlider>
+    <AwesomeSlider >
     <div className={classes.Slide}>1</div>
     <div className={classes.Slide}>2</div>
     <div className={classes.Slide}>3</div>
