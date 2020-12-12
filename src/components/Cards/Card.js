bfb4e8583b53287e8869d87c14f806e5e29b0622
Nikita <69010558+Kapibarra@users.noreply.github.com>
2020-12-12 15:43:52 +0300

    fixed card wrapper

--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -9,11 +9,11 @@ const Card = () => {
       <div className={classes.CardWrapper}>
         <CardItem title="Услуга" price="от 5000 рублей" content="text text text text text text text text text text text text" />
         <CardItem title="Услуга" price="от 5000 рублей" content="text text text text text text text text text text text text" />
+        </div>
+        <div className={classes.CardWrapper}>
         <CardItem title="Услуга" price="от 5000 рублей" content="text text text text text text text text text text text text" />
         <CardItem title="Услуга" price="от 5000 рублей" content="text text text text text text text text text text text text" />
-        <CardItem title="Услуга" price="от 5000 рублей" content="text text text text text text text text text text text text" />
-        <CardItem title="Услуга" price="от 5000 рублей" content="text text text text text text text text text text text text" />
-      </div>
+        </div>
       <div className={classes.Wrapper}>
         <Button name="Заказать" link="/" />
       </div>
